c9a952e512f5c3d05a1b5ccad1f29532f0b93314
Elodie Lefebvre <elodielefebvre@Elodies-MacBook-Pro.local>
2021-04-27 16:16:57 -0400

    final touch on layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,13 +15,13 @@ const App = () => {
         {appUser && appUser.email ? (
           <StyledContainer>
             <div>
-            <Avatar style={{width:'200px'}} src={appUser.photoURL} />
+              <Avatar style={{ width: "200px" }} src={appUser.photoURL} />
             </div>
             <Main>
-              <Text>Welcome back</Text>
-              </Main>
-              <Main>
-              <Span>{appUser.displayName}</Span>
+              <Text>
+                Welcome back 
+                <Span>{` ${appUser.displayName}`}</Span>
+              </Text>
             </Main>
             <Info>
               <Text>{appUser.email}</Text>
@@ -30,8 +30,8 @@ const App = () => {
           </StyledContainer>
         ) : (
           <StyledContainer>
-          <Text> Welcome !</Text>  
-          <Button onClick={signInWithGoogle}>Sign In</Button>
+            <Text> Welcome !</Text>
+            <Button onClick={signInWithGoogle}>Sign In</Button>
           </StyledContainer>
         )}
       </StyledHeader>
@@ -43,8 +43,6 @@ const App = () => {
 const StyledPageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-
- 
 `;
 
 const StyledHeader = styled.nav`
@@ -57,7 +55,7 @@ const StyledUserContainer = styled.div`
   display: flex;
   align-items: center;
   height: 50px;
-  justify-content:center;
+  justify-content: center;
 `;
 
 const StyledContainer = styled.div`
@@ -66,65 +64,64 @@ const StyledContainer = styled.div`
   padding: 14px;
   display: flex;
   flex-direction: column;
-  justify-content:center;
-  align-items:center;
-
+  justify-content: center;
+  align-items: center;
 `;
 
 const Main = styled.div``;
 const Info = styled.div``;
 const Button = styled.button`
-display: inline-block;
-padding: .75rem 1.25rem;
-border-radius: 10rem;
-
-text-transform: uppercase;
-font-size: 1rem;
-letter-spacing: .15rem;
-transition: all .3s;
-position: relative;
-overflow: hidden;
-color: #fff;
-z-index: 1;
-&:after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #0cf;
+  display: inline-block;
+  padding: 0.75rem 1.25rem;
   border-radius: 10rem;
-  z-index: -2;
-}
-&:before {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 0%;
-  height: 100%;
-  background-color: #097396;
-  transition: all .3s;
-  border-radius: 10rem;
-  z-index: -1;
-}
-&:hover {
 
-cursor:pointer;
-  &:before {
+  text-transform: uppercase;
+  font-size: 1rem;
+  letter-spacing: 0.15rem;
+  transition: all 0.3s;
+  position: relative;
+  overflow: hidden;
+  color: #fff;
+  z-index: 1;
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
     width: 100%;
-  }}
-
-`
+    height: 100%;
+    background-color: #0cf;
+    border-radius: 10rem;
+    z-index: -2;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background-color: #097396;
+    transition: all 0.3s;
+    border-radius: 10rem;
+    z-index: -1;
+  }
+  &:hover {
+    cursor: pointer;
+    &:before {
+      width: 100%;
+    }
+  }
+`;
 const Text = styled.p`
-font-size:150%;
+  font-size: 150%;
 `;
 const Image = styled.div`
-height:300px;
-width:300px;
+  height: 300px;
+  width: 300px;
 `;
 const Span = styled.span`
-font-size:160%;
+  color: #097396;
+  
 `;
 export default App;
